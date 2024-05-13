4bf0b43b2facfd45841b1718c053b1e1cadb8149
Lukowie <lukevdm64@gmail.com>
2024-05-12 22:52:19 -0400

    playing around with styles

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, useWindowDimensions, Dimensions, Text, View, Alert, Image, SafeAreaView, Button, Pressable } from 'react-native';
+import {Home, Settings, UserRound} from '@tamagui/lucide-icons';
+import { View, Text, Button } from 'tamagui';
+import appConfig from './tamagui.config';
+import { StyleSheet, useWindowDimensions, Dimensions, Alert, Image, SafeAreaView, Pressable } from 'react-native';
+import { TamaguiProvider, Theme } from '@tamagui/core';
 
 export default function App() {
   const handlePress = () => console.log("Text pressed");
@@ -11,52 +15,40 @@ export default function App() {
   }
 
   return (
+    <TamaguiProvider config={appConfig}>
+      <Theme name="dark">
     <View style={{
       backgroundColor: "white",
       flex: 1,
-      flexDirection: 'column',
+      flexDirection: 'row',
+      alignItems: 'flex-end',
       justifyContent: 'center',
+      paddingHorizontal: 10,
+      paddingBottom: 10,
     }}>
       <StatusBar style="auto" />
-      <SafeAreaView style={{
-        backgroundColor: "gold",
-        flex: 1,
-      }}>
-
-        <Text onPress={handlePress}>Open up App.js to start working on your app!</Text>
-
-      </SafeAreaView>
-      <View style={{
-        backgroundColor: "green",
-        flex: 1,
-        paddingBottom: 50,
-        flexDirection: 'row',
-        alignItems: 'flex-end',
-        justifyContent: 'space-evenly',
-      }}>
-
-        <Button title="Home" color="#f194ff" onPress={() =>
+        <Button icon={<Home/>} size='$5' title="Home" color="#f194ff" style={styles.navBtn} onPress={() =>
           Alert.alert("Hello", "message", [
             { text: "yes", onPress: () => console.log("yes") },
             { text: "no", onPress: () => console.log("no") },
           ])
-        } />
-        <Button title="You" color="#f194ff" onPress={() =>
+        }></Button>
+        <Button icon={<UserRound/>} size='$5' title="You" color="#f194ff" style={styles.navBtn} onPress={() =>
           Alert.alert("Hello", "message", [
             { text: "yes", onPress: () => console.log("yes") },
             { text: "no", onPress: () => console.log("no") },
           ])
-        } />
-        <Button title="Settings" color="#f194ff" onPress={() =>
+        }></Button>
+        <Button icon={<Settings/>} size='$5' title="Settings" color="#f194ff" style={styles.navBtn} onPress={() =>
           Alert.alert("Hello", "message", [
             { text: "yes", onPress: () => console.log("yes") },
             { text: "no", onPress: () => console.log("no") },
           ])
         }
-        />
-      </View>
-
-    </View >
+        ></Button>
+        </View>
+    </Theme>
+    </TamaguiProvider>
   );
 }
 
@@ -67,4 +59,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  navBtn: {
+    borderRadius: 0,
+    width: '33%',
+    marginBottom: 20,
+  }
 });
