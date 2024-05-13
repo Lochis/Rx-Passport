d6eb3821f5c0c193261d5df0126cf5b13c9a41ab
Lukowie <lukevdm64@gmail.com>
2024-05-12 20:49:23 -0400

    Watched tutorial, going to be working on app shortly.

--- App.js
+++ App.js
@@ -1,19 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, useWindowDimensions, Dimensions, Text, View, Alert, Image, SafeAreaView, Button, Pressable } from 'react-native';
 
 export default function App() {
+  const handlePress = () => console.log("Text pressed");
+  let orientation = "portrait";
+  if (useWindowDimensions.height > useWindowDimensions.length) {
+    orientation = "portrait";
+  } else {
+    orientation = "landscape";
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{
+      backgroundColor: "white",
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'center',
+    }}>
       <StatusBar style="auto" />
-    </View>
+      <SafeAreaView style={{
+        backgroundColor: "gold",
+        flex: 1,
+      }}>
+
+        <Text onPress={handlePress}>Open up App.js to start working on your app!</Text>
+
+      </SafeAreaView>
+      <View style={{
+        backgroundColor: "green",
+        flex: 1,
+        paddingBottom: 50,
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        justifyContent: 'space-evenly',
+      }}>
+
+        <Button title="Home" color="#f194ff" onPress={() =>
+          Alert.alert("Hello", "message", [
+            { text: "yes", onPress: () => console.log("yes") },
+            { text: "no", onPress: () => console.log("no") },
+          ])
+        } />
+        <Button title="You" color="#f194ff" onPress={() =>
+          Alert.alert("Hello", "message", [
+            { text: "yes", onPress: () => console.log("yes") },
+            { text: "no", onPress: () => console.log("no") },
+          ])
+        } />
+        <Button title="Settings" color="#f194ff" onPress={() =>
+          Alert.alert("Hello", "message", [
+            { text: "yes", onPress: () => console.log("yes") },
+            { text: "no", onPress: () => console.log("no") },
+          ])
+        }
+        />
+      </View>
+
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'orange',
     alignItems: 'center',
     justifyContent: 'center',
   },
